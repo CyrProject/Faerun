c8d1c7bb7964749ecf221bb6879f4b86806ce268
TJ <tjmatychuk@hotmail.com>
2017-08-13 13:52:24 -0500

    Added more content to the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,24 @@
 window.onload = function () {
     var map = L.map('map', {
-        crs: L.CRS.Simple
+        crs: L.CRS.Simple,
+        maxZoom: 2.5
     });
 
-    var bounds = [[0, 0], [1000, 1000]];
+    var bounds = [[0, 0], [1600, 2000]];
     var image = L.imageOverlay('./assets/image/mymap.png', bounds).addTo(map);
 
     map.fitBounds(bounds);
+
+    var yx = L.latLng;
+
+    var xy = function (x, y) {
+        if (L.Util.isArray(x)) {
+            return yx(x[1], x[0]);
+        }
+        return yx(y, x);
+    };
+
+    var neverwinter = xy(698, 1381);
+
+    L.marker(neverwinter).addTo(map).bindTooltip('Neverwinter');
 };
