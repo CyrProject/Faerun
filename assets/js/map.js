e3ddcb4bc04df1d19325409268d9bd5eb236927c
TJ <tjmatychuk@hotmail.com>
2017-08-13 10:28:47 -0500

    Wrap in document ready check

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,10 @@
-var map = L.map('map', {
-  crs: L.CRS.Simple
-});
+$(document).ready(function() {
+  var map = L.map('map', {
+    crs: L.CRS.Simple
+  });
 
-var bounds = [[0, 0], [1000, 1000]];
-var image = L.imageOverlay('/image/mymap.png', bounds).addTo(map);
+  var bounds = [[0, 0], [1000, 1000]];
+  var image = L.imageOverlay('/image/mymap.png', bounds).addTo(map);
 
-map.fitBounds(bounds);
+  map.fitBounds(bounds);
+}
