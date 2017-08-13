10b0d78f5f699712dfba7c3b9a54710d8a1ffd99
TJ <tjmatychuk@hotmail.com>
2017-08-13 10:37:13 -0500

    Fixed pathing to image file

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,10 @@
-window.onload = function() {
-  var map = L.map('map', {
-    crs: L.CRS.Simple
-  });
+window.onload = function () {
+    var map = L.map('map', {
+        crs: L.CRS.Simple
+    });
 
-  var bounds = [[0, 0], [1000, 1000]];
-  var image = L.imageOverlay('/image/mymap.png', bounds).addTo(map);
+    var bounds = [[0, 0], [1000, 1000]];
+    var image = L.imageOverlay('../image/mymap.png', bounds).addTo(map);
 
-  map.fitBounds(bounds);
+    map.fitBounds(bounds);
 };
