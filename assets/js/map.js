71d68d7fbc5e1b641458131fa5ba032979395e58
TJ <tjmatychuk@hotmail.com>
2017-08-15 18:32:01 -0500

    Added legend

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,22 +1,35 @@
 window.onload = function () {
-    var bounds = [[0, 0], [1000, 1500]];
+  var bounds = [[0, 0], [1000, 1500]];
 
-    var map = L.map('map', {
-        crs: L.CRS.Simple,
-        maxZoom: 2.5,
-        maxBounds: bounds
-    });
+  var map = L.map('map', {
+    crs: L.CRS.Simple,
+    maxZoom: 2.5,
+    maxBounds: bounds
+  });
 
-    var image = L.imageOverlay('./assets/image/mymap.png', bounds).addTo(map);
+  var image = L.imageOverlay('./assets/image/mymap.png', bounds).addTo(map);
 
-    map.fitBounds(bounds);
+  map.fitBounds(bounds);
 
-    var yx = L.latLng;
+  var yx = L.latLng;
 
-    var xy = function (x, y) {
-        if (L.Util.isArray(x)) {
-            return yx(x[1], x[0]);
-        }
-        return yx(y, x);
-    };
+  var xy = function (x, y) {
+    if (L.Util.isArray(x)) {
+      return yx(x[1], x[0]);
+    }
+    return yx(y, x);
+  };
+
+  var legend = L.control({position: 'bottomleft'});
+
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'legend');
+
+    div.innerHTML += '1 hex = 10 miles'
+
+    return div;
+  };
+
+  legend.addTo(map);
 };
