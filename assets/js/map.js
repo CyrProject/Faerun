3c692114b185ffb7a9a04358f81a503877c75f26
TJ <tjmatychuk@hotmail.com>
2017-08-19 18:59:27 -0500

    Started tiling images

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,21 +11,29 @@ window.onload = function () {
         return yx(y, x);
     };
 
-    var bounds = [[0, 0], [994, 1500]];
+    var bounds = [[0, 0], [-0.00538, 0.00812]];
 
     var map = L.map('map', {
         crs: L.CRS.Simple,
-        center: [840, 530],
-        maxZoom: 2.5,
-        minZoom: 2.5,
-        maxBounds: bounds
+        center: [0, 0],
+        // maxZoom: 2.5,
+        minZoom: 17,
+        maxBounds: bounds,
+        // maxZoom: 20,
+        // minZoom: 20,
+        zoom: 20
     });
 
-    var image = L.imageOverlay('./assets/image/mymapnolabels-min.png', bounds).addTo(map);
+    // var image = L.imageOverlay('./assets/image/mymapnolabels-min.png', bounds).addTo(map);
 
-    map.fitBounds(bounds);
+    L.tileLayer('./assets/image/image-tiles/{z}/map_tile_{x}_{y}.png', {
+      tileSize: 256,
+      maxZoom: 20
+    }).addTo(map);
 
-    map.setView([840, 530], 2.5);
+    // map.fitBounds(bounds);
+
+    map.setView([-0.00107, 0.00302], 20);
 
     var legend = L.control();
 
@@ -47,9 +55,15 @@ window.onload = function () {
         popupAnchor:  [5, -12]
     });
 
-    L.marker([807, 522], {icon: excamationIcon}).addTo(map).bindPopup('Neverwinter');
-    L.marker([836, 535], {icon: excamationIcon}).addTo(map).bindPopup('Mount Hotenow');
-    L.marker([830, 535], {icon: excamationIcon}).addTo(map).bindPopup('Sunless Citadel');
-    L.marker([824, 532], {icon: excamationIcon}).addTo(map).bindPopup('Oakhurst');
-    L.marker([881, 504], {icon: excamationIcon}).addTo(map).bindPopup('Luskan');
+    // L.marker([807, 522], {icon: excamationIcon}).addTo(map).bindPopup('Neverwinter');
+    L.marker([-0.00085, 0.002899], {icon: excamationIcon}).addTo(map).bindPopup('Mount Hotenow');
+    // L.marker([830, 535], {icon: excamationIcon}).addTo(map).bindPopup('Sunless Citadel');
+    // L.marker([824, 532], {icon: excamationIcon}).addTo(map).bindPopup('Oakhurst');
+    // L.marker([881, 504], {icon: excamationIcon}).addTo(map).bindPopup('Luskan');
+
+//     function onMapClick(e) {
+//     alert("You clicked the map at " + e.latlng);
+// }
+//
+// map.on('click', onMapClick);
 };
